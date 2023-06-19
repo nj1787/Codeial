75dcb6b73aaefc7e305ccff52650d02361610508
nj1787 <neerajj9@gmail.com>
2023-06-19 23:54:54 +0530

    User Schema Created. Mannual Authentication Process Initiated.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,17 @@ module.exports.profile = function (req, res) {
     title: "Profile",
   });
 };
+
+//Render Sign Up Page
+module.exports.signUp = function (req, res) {
+  return res.render("signup", {
+    title: "Codeial || Sign Up",
+  });
+};
+
+//Render Sign In Page
+module.exports.signIn = function (req, res) {
+  return res.render("signin", {
+    title: "Codeial || Sign In",
+  });
+};
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,4 +6,8 @@ const usersController = require("../controllers/users_controller");
 
 router.get("/profile", usersController.profile);
 
+router.get("/signin", usersController.signIn);
+
+router.get("/signup", usersController.signUp);
+
 module.exports = router;
