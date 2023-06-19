7b585f76a5ac83e3f2680637ba02e04da75e1c8b
nj1787 <neerajj9@gmail.com>
2023-06-19 22:04:02 +0530

    View Engine Setup Complete.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 7500;
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(expressLayouts);
 
 //use express router
 app.use("/", require("./routes/routes"));
+
+//set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, (err) => {
   if (err) {
