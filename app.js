9aa0af8771ff22303c8547bbe10acd814348014a
nj1787 <neerajj9@gmail.com>
2023-06-19 19:36:22 +0530

    Express Router Setup Complete.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 7500;
 
+//use express router
+app.use("/", require("./routes/routes"));
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error Occured: ${err}`);
