5e40875e0014c3436c448eb0d04df27b0ec45da1
nj1787 <neerajj9@gmail.com>
2023-06-21 19:39:30 +0530

    Added Authentication Checks For SignUp and SignIn.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,9 @@ module.exports.profile = function (req, res) {
 
 //Render Sign Up Page
 module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("signup", {
     title: "Codeial || Sign Up",
   });
@@ -15,6 +18,9 @@ module.exports.signUp = function (req, res) {
 
 //Render Sign In Page
 module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("signin", {
     title: "Codeial || Sign In",
   });
