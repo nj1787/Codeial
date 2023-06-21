fa186e28c5d3f32706ed93d1c058c314031d2116
nj1787 <neerajj9@gmail.com>
2023-06-21 20:16:35 +0530

    Persistent Session Storage Successfull.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo")(session);
 
 app.use(express.urlencoded());
 
@@ -24,6 +25,7 @@ app.set("layout extractScripts", true);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+//mongo-store is used to store the session cookie in the db
 app.use(
   session({
     name: "codeial",
@@ -34,6 +36,15 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 100,
     },
+    store: new MongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
   })
 );
 
