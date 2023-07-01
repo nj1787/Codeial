f783ecc71a04b4af7da935978a28dac23a945bdd
nj1787 <neerajj9@gmail.com>
2023-07-01 23:29:55 +0530

    Check Authentication Before Creating A Post.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,6 +3,9 @@ const Post = require("../models/post");
 module.exports.home = function (req, res) {
   // console.log(req.cookies);
   // res.cookie("user_id", 25);
+
+  //Only Populate The Post
+
   // Post.find({}).then((posts) => {
   //   return res.render("home", {
   //     title: "Codeial | Home",
