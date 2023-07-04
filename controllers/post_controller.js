9ffd8fb67e48e087bcfbb0a960798d0a7a90369d
nj1787 <neerajj9@gmail.com>
2023-07-05 00:11:23 +0530

    Post and It's Comments Deletion Successful.

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
 module.exports.create = function (req, res) {
   Post.create({
@@ -11,5 +12,30 @@ module.exports.create = function (req, res) {
     .catch((err) => {
       console.log(`Error Occured While Creating A Post: ${err}`);
       return;
+    });
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id)
+    .then((post) => {
+      //.id means converting the object id into string
+      if (post.user == req.user.id) {
+        //post.remove() -- Deprecated
+        post.deleteOne();
+
+        Comment.deleteMany({ post: req.params.id })
+          .then(() => {
+            res.redirect("back");
+          })
+          .catch((err) => {
+            console.log(`Error Occured While Deleting The Post: ${err}`);
+            return;
+          });
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log(`Some Error Occured : ${err}`);
     });
 };
