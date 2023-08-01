5f7be9241bc30295277521bb8e4c823cf16fcfe8
nj1787 <neerajj9@gmail.com>
2023-08-01 13:26:29 +0530

    Configured Multer and Saved File From Params.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,20 +13,53 @@ module.exports.profile = function (req, res) {
     });
 };
 
-module.exports.update = function (req, res) {
-  if (req.params.id == req.user.id) {
-    User.findByIdAndUpdate(req.params.id, req.body)
-      .then((user) => {
+//Async Await
+module.exports.update = async function (req, res) {
+  if (req.user.id === req.params.id) {
+    try {
+      let user = await User.findById(req.params.id);
+      User.uploadedAvatar(req, res, function (err) {
+        if (err) {
+          console.log("********Multer Error*********", err);
+        }
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if (req.file) {
+          //this is saving the path of the uploaded file into the avatar field in the user
+          user.avatar = User.avatarPath + "/" + req.file.fileName;
+        }
+
+        user.save();
+
         return res.redirect("back");
-      })
-      .catch((err) => {
-        console.log("Some Error Occured", err);
-        return;
       });
+    } catch (err) {
+      req.flash("error", err);
+      return res.redirect("back");
+    }
   } else {
+    req.flash("error", "Unauthorized");
     return res.status(401).send("Unauthorized");
   }
 };
+
+//Legacy Code
+
+// module.exports.update = function (req, res) {
+//   if (req.params.id == req.user.id) {
+//     User.findByIdAndUpdate(req.params.id, req.body)
+//       .then((user) => {
+//         return res.redirect("back");
+//       })
+//       .catch((err) => {
+//         console.log("Some Error Occured", err);
+//         return;
+//       });
+//   } else {
+//     return res.status(401).send("Unauthorized");
+//   }
+// };
 
 //Render Sign Up Page
 module.exports.signUp = function (req, res) {
